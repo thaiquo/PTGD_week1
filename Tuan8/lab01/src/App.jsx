4d05d0fbf828cc68358a6c8c4445379c3e427122
thaiquo <ngocquocthai.004@gmail.com>
2025-04-16 07:44:31 +0700

    Tuan 8_ MainSection

--- Tuan8/lab01/src/App.jsx
+++ Tuan8/lab01/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header';
+import MainSection from './components/MainSection';
 
 function App() {
   return (
     <div style={{ fontFamily: 'Arial, sans-serif', textAlign: 'center' }}>
           <Header />
+          <MainSection/>
          
         </div>
       );
---
+++ Tuan8/lab01/src/components/MainSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MainSection = () => {
+  return (
+    <div>
+      <img
+        src="HinhAnh/kitchen.png"
+        alt="Cooking Scene"
+        style={{ width: '100%', height: '400px', objectFit: 'cover' }}
+      />
+    </div>
+  );
+};
+
+export default MainSection;
